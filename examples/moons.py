0864d0556f22363b4103468399e4cd70433b69b2
Ryan Meredith <ryan@ryanmeredith.com>
2025-12-19 20:49:55 -0500

    Moons example

---
+++ examples/moons.py
@@ -0,0 +1,46 @@
+from keras import Sequential
+from keras.layers import Dense, Input
+from keras.optimizers import Adam
+from sklearn.datasets import make_moons
+
+
+def build_model(
+    num_layers,
+    layer_width,
+    jit_compile="auto",
+    kernel_regularizer=None,
+    learning_rate=0.001,
+    batch_size=32,
+    epochs=100,
+):
+    model = Sequential()
+    model.add(Input(batch_shape=(batch_size, 2)), rebuild=False)
+    for _ in range(num_layers):
+        model.add(
+            Dense(
+                layer_width,
+                activation="relu",
+                kernel_regularizer=kernel_regularizer,
+            ),
+            rebuild=False,
+        )
+    model.add(
+        Dense(
+            1,
+            activation="sigmoid",
+            kernel_regularizer=kernel_regularizer,
+        )
+    )
+
+    model.compile(
+        optimizer=Adam(learning_rate),
+        loss="binary_crossentropy",
+        metrics=["accuracy"],
+        jit_compile=jit_compile,
+    )
+
+    x, y = make_moons(noise=0.05, random_state=42)
+
+    history = model.fit(x, y, batch_size, epochs, validation_split=0.2)
+
+    return model, history
