2376f5b0091d6b40f3c3c62b8ab134f66c8dcac8
Ryan Meredith <ryan@ryanmeredith.com>
2025-12-21 09:44:41 -0500

    Improved neural persistence implementation

--- persistence_reg.py
+++ persistence_reg.py
@@ -1,18 +1,18 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 from math import inf
 
 from keras import Regularizer
 from keras.ops import (
-    append,
     argmax,
     array,
     cond,
     fori_loop,
+    full,
     max,
+    maximum,
     shape,
     slice_update,
     sum,
-    where,
     zeros,
 )
 
@@ -21,32 +21,28 @@ def _maximum_spanning_tree(weights):
     m, n = shape(weights)
     num_nodes = m + n
 
-    max_weights = array((0.0,) + (-inf,) * (num_nodes - 1))
-    visited = zeros(num_nodes, dtype=bool)
+    max_weights = full((num_nodes,), -inf)
+    max_weights = slice_update(max_weights, (0,), array((0,)))
+    visited_mask = zeros(num_nodes)
 
-    initial_state = max_weights, visited
+    initial_state = max_weights, visited_mask
 
     def body(_, state):
-        max_weights, visited = state
-        i = argmax(where(visited, -inf, max_weights))
-
-        new_visited = slice_update(visited, (i,), array((True,)))
+        max_weights, visited_mask = state
+        i = argmax(max_weights + visited_mask)
 
         def input_node():
-            potential_weights = weights[i, :]
-            do_update = (potential_weights > max_weights[m:]) & ~visited[m:]
-            update = where(do_update, potential_weights, max_weights[m:])
-            return append(max_weights[:m], update)
+            update = maximum(weights[i, :] + visited_mask[m:], max_weights[m:])
+            return slice_update(max_weights, (m,), update)
 
         def output_node():
-            potential_weights = weights[:, i - m]
-            do_update = (potential_weights > max_weights[:m]) & ~visited[:m]
-            update = where(do_update, potential_weights, max_weights[:m])
-            return append(update, max_weights[m:])
+            update = maximum(weights[:, i - m] + visited_mask[:m], max_weights[:m])
+            return slice_update(max_weights, (0,), update)
 
         new_max_weights = cond(i < m, input_node, output_node)
+        new_visited_mask = slice_update(visited_mask, (i,), array((-inf,)))
 
-        return new_max_weights, new_visited
+        return new_max_weights, new_visited_mask
 
     final_max_weights, _ = fori_loop(0, num_nodes, body, initial_state)
 
@@ -55,11 +51,14 @@ def _maximum_spanning_tree(weights):
 
 @dataclass
 class NeuralPersistence(Regularizer):
-    norm: int = 2
     scale: float = 1.0
+    norm: int = 2
 
     def __call__(self, weights):
         absolute_weights = abs(weights)
         normalized = absolute_weights / max(absolute_weights)
         persistence = 1 - _maximum_spanning_tree(normalized)
         return -self.scale * sum(persistence**self.norm) ** (1 / self.norm)
+
+    def get_config(self):
+        asdict(self)
